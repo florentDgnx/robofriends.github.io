b375002a62ee1d6468ee113569d6c7b436a0105f
DAGNIAUX Florent <florent.dagniaux@soprasteria.com>
2019-10-10 11:59:09 +0200

    Updates

---
+++ precache-manifest.080c61af3ef519c6fc0f6eb4f354d54c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d0959cb8fd787a34c94a0ae4301a31",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c85dab1ae8c68ab13ea1",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e66fc848f9e6ccebc971",
+    "url": "/robofriends/static/css/main.302f2549.chunk.css"
+  },
+  {
+    "revision": "c85dab1ae8c68ab13ea1",
+    "url": "/robofriends/static/js/2.999231fa.chunk.js"
+  },
+  {
+    "revision": "e66fc848f9e6ccebc971",
+    "url": "/robofriends/static/js/main.c5afef67.chunk.js"
+  },
+  {
+    "revision": "cb519fe66fcbb61e7a5c",
+    "url": "/robofriends/static/js/runtime-main.0560b89d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
